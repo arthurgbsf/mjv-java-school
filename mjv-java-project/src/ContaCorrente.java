ffd137a93278578a226c1135e484269f403594ec
arthurgbsf <arthurgbsf@gmail.com>
2023-06-13 18:36:54 -0300

    java exercise - 12-06-23

---
+++ mjv-java-project/src/ContaCorrente.java
@@ -0,0 +1,14 @@
+
+public class ContaCorrente {
+    
+    Cliente cliente;
+    Double saldo;
+    Integer numeroConta;
+    Integer numeroAgencia;
+    boolean ativa=true;
+    
+    Double cs(){
+        return 0.0;
+    }
+
+}
