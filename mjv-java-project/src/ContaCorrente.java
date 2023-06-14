fbd2fca274b83ca23071de41a73bd12949bda06b
arthurgbsf <arthurgbsf@gmail.com>
2023-06-14 19:14:58 -0300

    Adição de novas classes

--- mjv-java-project/src/ContaCorrente.java
+++ mjv-java-project/src/ContaCorrente.java
@@ -1,14 +1,43 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContaCorrente {
     
     Cliente cliente;
-    Double saldo;
+    Double saldo = 0.0;
     Integer numeroConta;
     Integer numeroAgencia;
     boolean ativa=true;
+    List<Transacao> transacoes = new ArrayList<>();
     
-    Double cs(){
-        return 0.0;
+    private void incluirTransacao(Double valor, String tipo, String descricao){
+        Transacao t = new Transacao();
+        t.data = LocalDate.now();
+        t.tipo= tipo;
+        t.descricao = descricao;
+        t.valor = valor;
+        transacoes.add(t);
+    }
+   
+    boolean sacar(double valor){
+
+        if(saldo < valor){
+        return false;  
+        } 
+        saldo -= valor;
+        incluirTransacao(saldo, "Saque", "Saque em conta corrente");
+        return true;
+
+    }
+
+    Double consultarSaldo(){
+        return saldo;
+    }
+
+    List<Transacao> consultarExtrato(LocalDate di, LocalDate df){
+
+       
     }
 
 }
---
+++ mjv-java-project/src/Transacao.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class Transacao {
+    
+    LocalDate data;
+    String descricao;
+    Double valor;
+    String tipo; 
+
+}
