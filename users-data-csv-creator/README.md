db34fc1a413aab8b151052bb50d66d2a0285d942
arthurgbsf <arthurgbsf@gmail.com>
2023-06-18 20:00:05 -0300

    Working on CsvBuilder.java

---
+++ users-data-csv-creator/README.md
@@ -0,0 +1,6 @@
+<h1> User's data CSV creator </h1>
+
+
+
+## About
+
---
+++ users-data-csv-creator/src/App.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+import model.User.User;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        ArrayList<User> users = new ArrayList<>();
+        
+        CsvBuider csvBuilder = new csvBuilder(users);
+
+    }
+}
